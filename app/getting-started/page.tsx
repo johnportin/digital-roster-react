97c759e46dab70fe5c6094c49ecc7bca5e58f5d4
John Portin <johnportin@Johns-MacBook-Pro.local>
2023-07-10 10:27:24 -0500

    Add add course form

---
+++ app/getting-started/page.tsx
@@ -0,0 +1,5 @@
+const GettingStartedPage = () => {
+  return <div>getting-started</div>;
+};
+
+export default GettingStartedPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>This is the main page.</p>
+    <main className="flex max-w-2xl m-auto min-h-screen flex-col items-center p-24 gap-4 text-center">
+      <h1 className="text-7xl">Digi-Roster</h1>
+      <h2 className="text-2xl">A modern solution for an acient problem</h2>
+      <p className="text-md">
+        Digi-Roster enables anyone to track attendance for school courses,
+        event, or whatever you need!
+      </p>
+      <div className="flex flex-row gap-4">
+        <Button onClick={() => router.push('/getting-started')}>
+          Getting Started
+        </Button>
+        <Button onClick={() => router.push('/dashboard')}>Dashboard</Button>
+      </div>
     </main>
   );
 }
